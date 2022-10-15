e7b52c098f3911d921740fbf4cd82d703bf4b5dc
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-10-15 09:10:45 -0300

    Commit
    Feat: add string connection

---
+++ ConsumoDeVeiculos/Models/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Second_MVC.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor informe o nome do veiculo")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a placa do veiculo")]
+        public string Placa { get; set; }
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
